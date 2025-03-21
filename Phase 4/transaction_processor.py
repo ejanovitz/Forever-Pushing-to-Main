4170fa9badbd0f987ee62e3cc6e9809cd7008a36
Kershan Arulneswaran <74938655+Koops0@users.noreply.github.com>
2025-03-21 13:18:17 -0400

    added more comments for thought process

--- Phase 4/transaction_processor.py
+++ Phase 4/transaction_processor.py
@@ -1,7 +1,7 @@
 # This is the transaction processor class, which will calculate the total cost of transactions.
 class TransactionProcessor:
     # Constructor
-    def __init__(self, ):
+    def __init__(self):
         self.file = None
     
     # This function calculates the total cost of transactions and returns a dict.
@@ -10,11 +10,15 @@ class TransactionProcessor:
         self.file = transaction_file
         transaction_dict = {'SP': 0.00, 'NP': 0.00}
 
-        # Now, we have to write this
+        # Now, we have to read this file in order to do caluclations
         with open(self.file, 'r') as file:
-            # Parse each line
+            # In order to calculate, we first reead every line
             for line in file:
+                # Then, we get rid of potential white spaces
+                line = line.strip()
                 plan = line[-2:]
+
+                # Calculations
                 if plan == 'SP':
                     transaction_dict['SP'] += 0.05
                 else:
